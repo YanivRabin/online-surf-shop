5f328edd08b9e935fb906727384629397437c2d0
Roytol <roey.toledo@gmail.com>
2023-06-12 18:28:53 +0300

    Implement Chat

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,11 +1,17 @@
+
+
 const path = require('path');
 
 const getHomePage = (req, res) => {
     res.sendFile(path.join(__dirname, '../views/home.html'));
+};
+const getChat = (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/chat.html'));
 };
 
 
 module.exports = {
 
     getHomePage,
+    getChat
 }
