1ed39d88aa4b942664676058f9e36fd7df5af564
Roytol <roey.toledo@gmail.com>
2023-06-12 16:31:34 +0300

    Chat Try

--- chat/app.js
+++ chat/app.js
@@ -1,21 +1,21 @@
-// const app = require('express')();
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
-//
-// app.get('/chat', (req, res) => {
-//     res.sendFile(__dirname + '/chat.html');
-// });
-//
-// io.on('connection', (req,res,socket) => {
-//     socket.broadcast.emit('joined', socket.username+' Joined');
-//
-//     socket.on('disconnect', () => {
-//         socket.broadcast.emit('disconnected', socket.username+' Disconnected');
-//     });
-//
-//     socket.on('new message', (msg) => {
-//         io.emit('new message', socket.username + ': ' + msg);
-//     });
-// });
-//
-// http.listen(3000);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+ 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/app.html');
+}); 
+ 
+io.on('connection', (req,res,socket) => {
+    socket.broadcast.emit('joined', socket.username+' Joined');
+ 
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disconnected', socket.username+' Disconnected');
+    });
+
+    socket.on('new message', (msg) => {
+        io.emit('new message', socket.username + ': ' + msg);
+    });
+});
+ 
+http.listen(3000);
