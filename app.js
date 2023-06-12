ab1be4167f86b5befe494975239801d8c6d8ffa9
Roytol <roey.toledo@gmail.com>
2023-06-12 18:22:22 +0300

    Implement Chat

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const { mongoose }   = require('mongoose');
-const path           = require("path");
-const session        = require("express-session");
-const socketIO       = require('socket.io')
-const http           = require('http')
-const homeRouter     = require('./routes/HomeRouter');
-const productsRouter = require('./routes/ProductsRouter');
-const authRouter     = require('./routes/AuthRouter')
+const express               = require('express');
+const bodyParser            = require('body-parser');
+const { mongoose }          = require('mongoose');
+const path                  = require("path");
+const session               = require("express-session");
+const socketIO = require('socket.io')
+const http = require('http')
+
+
+
+const homeRouter            = require('./routes/HomeRouter');
+const productsRouter        = require('./routes/ProductsRouter');
+const authRouter            = require('./routes/AuthRouter')
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const server=http.createServer(app);
+const io=socketIO(server);
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -31,10 +34,13 @@ app.use(session({
 app.use('/', homeRouter);
 app.use('/auth', authRouter); // for login and register
 app.use('/store', productsRouter); // for surfboards and other products
+// app.use('/surfboards', surfboardsRouter);
+app.use('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/chat.html'));
+});
 
 io.on('connection', (socket) => {
-
-    socket.broadcast.emit('joined', socket.username + ' Joined');
+    socket.broadcast.emit('joined', socket.username+' Joined');
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('disconnected', socket.username+' Disconnected');
@@ -49,3 +55,9 @@ io.on('connection', (socket) => {
 server.listen(3000, () => { console.log('Server is running on http://localhost:3000'); });
 // npm run app
 // for test: npm run test
+
+module.exports = {
+    io: io,
+   server: server,
+   app: app
+}
