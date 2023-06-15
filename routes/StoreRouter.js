c8ae26baa798551d869a83591f784e106666752a
Roytol <roey.toledo@gmail.com>
2023-06-15 14:10:52 +0300

    Hide the Google secret key

--- routes/StoreRouter.js
+++ routes/StoreRouter.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const Store = require('../models/StoreModel');
 const path = require('path');
-const fs = require('fs'); // Add this line
+const fs = require('fs');
+const {API_KEY} = require("../config/config"); // Add this line
 const router = express.Router();
+const apiKey = API_KEY;
+
 
 router.get('/', async (req, res) => {
     try {
         const branches = await Store.find();
         let dataScript = `<script>let branches = ${JSON.stringify(branches)};</script>`;
         let html = fs.readFileSync(path.join(__dirname, '../views/map.html'), 'utf-8');
-        html = html.replace('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkVJDbLybIIsME5xfeB2YwkxIgX9-kgz4&callback=initMap" async defer></script>'
-            , dataScript + '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkVJDbLybIIsME5xfeB2YwkxIgX9-kgz4&callback=initMap" async defer></script>');
+        html = html.replace('<script src="https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap" async defer></script>'
+            , dataScript + '<script src="https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap" async defer></script>');
         res.send(html);
     } catch (err) {
         console.error(err);
