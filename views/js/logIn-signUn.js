5ff8ac5598721f7582d71dd0b770f5e5a2c0c9ae
Tal Avraham <talavraham2@cs.colman.ac.il>
2023-07-21 20:04:16 +0300

    updates

--- views/js/logIn-signUn.js
+++ views/js/logIn-signUn.js
@@ -1,14 +1,15 @@
 $(document).ready(() => {
 
-    $('#signIn').click((event) => {
+    $('#signUp').click((event) => {
         event.preventDefault();
         $.ajax({
             url: '/auth/register',
             method: 'POST',
             data: $('#login').serialize(),
             success: (response) => {
+
                 // Registration successful
-                alert('Registration successful!');
+                alert('Registration successful! Welcome ');
                 window.location.href = '/';
             },
             error: (xhr) => {
@@ -18,42 +19,61 @@ $(document).ready(() => {
         });
     });
 
+    $('#loginIcon').click((event) => {
+        event.preventDefault();
+        // Redirect the user to the login page
+        window.location.href = 'login.html';
+    });
+
     $('#login').submit((event) => {
         event.preventDefault();
+        const username = $('#username').val();
+
         $.ajax({
             url: '/auth/login',
             method: 'POST',
             data: $('#login').serialize(),
             success: (response) => {
-
                 const { username } = response;
                 sessionStorage.setItem('username', username);
 
-                alert('Welcome '+username +'!!');
-                $('#loginLink').text('Logout');
-                $('#loginLink').off('click');
-                $('#loginLink').click((event) => {
+                // Display a welcome message
+                alert('Welcome ' + username + '!!');
+                $('#loginIcon').html('<i class="fa fa-user-circle" style="transform: scale(1.15);color: rgb(85,121,171);"></i> ' + username);
+                // Hide the login form (assuming it has an ID "loginForm")
+                $('#loginForm').hide();
+
+
+                // Add the logout click event handler
+                $('#loginStatus').click((event) => {
                     event.preventDefault();
+                    event.preventDefault(); // Prevent form submission
                     $.ajax({
+
                         url: '/auth/logout',
                         method: 'POST',
                         success: () => {
-                            alert('Logout successful!');
+
                             window.location.href = '/';
                         },
                         error: (xhr) => {
+
                             const { message } = xhr.responseJSON;
-                            alert('Logout Error: ' + message);
+                            alert('Status: ' + xhr.status + '\nMessage: ' + message);
                         }
                     });
                 });
+
+                // Redirect the user to the home page after successful login
                 window.location.href = '/';
             },
+
             error: (xhr) => {
                 const { message } = xhr.responseJSON;
                 alert('Login Error: ' + message);
             }
         });
     });
+
 
 });
